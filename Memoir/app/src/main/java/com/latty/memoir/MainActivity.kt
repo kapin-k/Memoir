444981ae3eac30370168f6f614749a3faf307635
kapin-k <kkapindran@gmail.com>
2020-07-10 01:00:10 -0400

    added scratch card + memory model

--- Memoir/app/src/main/java/com/latty/memoir/MainActivity.kt
+++ Memoir/app/src/main/java/com/latty/memoir/MainActivity.kt
@@ -2,11 +2,20 @@ package com.latty.memoir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import jp.wasabeef.glide.transformations.BlurTransformation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide.with(this).load(ContextCompat.getDrawable(this, R.drawable.reveal_picture_1))
+            .apply(RequestOptions.bitmapTransform(BlurTransformation(10, 5)))
+            .into(backgroundImageView)
     }
 }
---
+++ Memoir/app/src/main/java/com/latty/memoir/MemoryModel.kt
@@ -0,0 +1,10 @@
+package com.latty.memoir
+
+import java.util.*
+
+data class MemoryModel(
+    val date: Date, // Image date Calendar.Date Format
+    val header: String,
+    val picture: Int, // R.drawable.* -> Image resource ID
+    val caption: String
+)
