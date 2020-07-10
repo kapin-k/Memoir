dc38074fc9e78587a506f5f9755f528314a9bb31
kapin-k <kkapindran@gmail.com>
2020-07-10 01:39:08 -0400

    before adding stack layout

--- Memoir/app/src/main/java/com/latty/memoir/MainActivity.kt
+++ Memoir/app/src/main/java/com/latty/memoir/MainActivity.kt
@@ -2,9 +2,12 @@ package com.latty.memoir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import dev.skymansandy.scratchcardlayout.listener.ScratchListener
+import dev.skymansandy.scratchcardlayout.ui.ScratchCardLayout
 import jp.wasabeef.glide.transformations.BlurTransformation
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,8 +17,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Background Image
         Glide.with(this).load(ContextCompat.getDrawable(this, R.drawable.reveal_picture_1))
-            .apply(RequestOptions.bitmapTransform(BlurTransformation(10, 5)))
+            .apply(RequestOptions.bitmapTransform(BlurTransformation(10, 3)))
             .into(backgroundImageView)
+
+        // Scratch Card Variables
+        scratchCard.setScratchWidthDip(80f)
+//        scratchCard.setScratchEnabled(true)
+//        scratchCard.setScratchListener(object : ScratchListener {
+//            override fun onScratchComplete() {
+//                scratchCard.resetScratch()
+//            }
+//
+//            override fun onScratchProgress(
+//                scratchCardLayout: ScratchCardLayout,
+//                atLeastScratchedPercent: Int
+//            ) {
+//                if (atLeastScratchedPercent > 50) scratchCard.revealScratch()
+//            }
+//
+//            override fun onScratchStarted() {
+//            }
+//        })
+
+        
+
     }
 }
--- Memoir/app/src/main/java/com/latty/memoir/MemoryModel.kt
+++ Memoir/app/src/main/java/com/latty/memoir/MemoryModel.kt
@@ -3,8 +3,8 @@ package com.latty.memoir
 import java.util.*
 
 data class MemoryModel(
-    val date: Date, // Image date Calendar.Date Format
-    val header: String,
+    val date: Date?, // Image date Calendar.Date Format
+    val header: String?,
     val picture: Int, // R.drawable.* -> Image resource ID
     val caption: String
 )
